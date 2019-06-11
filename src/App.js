ee7464fa9c1e93d458c697413c9f143f616dd546
Harry Heckmann <heckmann.harry@gmail.com>
2019-06-11 10:26:13 -0500

    updated files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      showNav: false
+    }
+  }
+
+  showMenu(){
+    this.setState({showNav: !this.state.showNav})
+  }
+ 
+  render(){
+    const {showNav} = this.state
+    return (
+      <div className="App">
+         <nav id='full_nav'>
+            <h2>Home</h2>
+            <h2>History</h2>
+            <h2>Explore</h2>
+            <h2>Contact</h2>
+         </nav>
+         <button id='nav_button' onClick={() => this.showMenu()}>Menu</button>
+         <nav id={showNav ? 'mobile_nav' : 'mobile_nav_hidden'}>
+            <div>
+              <h2>Home</h2>
+              <h2>History</h2>
+              <h2>Explore</h2>
+              <h2>Contact</h2>
+            </div>
+         </nav>
+         <div className='middle'>
+            <h1> “All good things are wild and free.” </h1>
+            <h3>-Henry David Thoreau</h3>
+         </div>
+
+      </div>
+    );
+  }
 }
 
 export default App;
